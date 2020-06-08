0e17586f03cb89e796bd13a3f96d44d21ac53975
陈小黑 <191714375@qq.com>
2020-06-08 16:50:08 +0800

    refactor: 删除无用代码

--- json_tool/js/index.js
+++ json_tool/js/index.js
@@ -2,10 +2,4 @@
  * 载入
  */
 $(function () {
-    $('#btn_showMask').on('click', function () {
-        showMask();
-    });
-    $('#btn_hideMask').on('click', function () {
-        hideMask();
-    });
 });
