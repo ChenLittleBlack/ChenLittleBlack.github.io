2e32304395efd8b4424da902a702f1953418376c
ChenLittleBlack <191714375@qq.com>
2023-06-02 15:51:27 +0800

    feat: Github地址

--- src/pages/Menu/index.vue
+++ src/pages/Menu/index.vue
@@ -22,8 +22,7 @@ export default {
       menus: [
         { text: '让我帮你百度一下', icon: 'search' },
         { text: '每日一宠', icon: 'smile-o', routeName: 'RandomPetPic', badge: 'Hot' },
-        { text: '空', icon: 'add-o' },
-        { text: '空', icon: 'add-o' },
+        { text: 'Github', icon: 'smile-o', url: 'https://github.com/ChenLittleBlack/ChenLittleBlack.github.io', badge: 'Home' },
         { text: '空', icon: 'add-o' },
         { text: '空', icon: 'add-o' },
         { text: '空', icon: 'add-o' },
